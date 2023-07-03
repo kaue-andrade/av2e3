683161fa2551f67585328033391a72e8a7ec5b2d
Kauê Andrade <91803383+kaue-andrade@users.noreply.github.com>
2023-07-03 17:32:14 -0300

    Fila

--- av2e3/src/Fila.java
+++ av2e3/src/Fila.java
@@ -1,2 +1,80 @@
-public class Fila {
+import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
+
+class NoFila {
+
+    String dados;
+    NoFila proximo;
+
+    public NoFila(String dados){
+        this.dados = dados;
+        this.proximo = null;
+    }
+}
+
+class Fila {
+    NoFila frente;
+    NoFila traz;
+
+    public Fila(){
+        this.frente = null;
+        this.traz = null;
+    }
+
+    public void enfileirar(String dados){
+        NoFila novoNo = new NoFila(dados);
+
+        if (traz == null){
+            frente = novoNo;
+            traz = novoNo;
+        } else {
+            traz.proximo = novoNo;
+            traz = novoNo;
+        }
+    }
+
+    public String desinfileirar(){
+        if (frente == null){
+            throw new NoSuchElementException();
+        }
+
+        String dados = frente.dados;
+
+        if (frente == traz){
+            frente = null;
+            traz = null;
+        } else {
+            frente = frente.proximo;
+        }
+
+        return dados;
+    }
+
+    public String peek(){
+        if(frente == null){
+            throw new NoSuchElementException();
+        }
+
+        return frente.dados;
+    }
+
+    public boolean estaVazia(){
+        return frente == null;
+    }
+
+    public static void main(String[] args) {
+        Fila f1 = new Fila();
+
+        f1.enfileirar("1");
+        f1.enfileirar("2");
+        f1.enfileirar("3");
+
+        System.out.println("Frente da fila: " + f1.peek() + "\n");
+
+        //f1.imprimirFila();
+
+        while (!f1.estaVazia()){
+            System.out.println("Desinfileirando: " + f1.desinfileirar());
+        }
+    }
 }
