d4c7b9b4685874693c63e72d2bb48bd3b9b50db1
Kauê Andrade <91803383+kaue-andrade@users.noreply.github.com>
2023-07-03 15:18:49 -0300

    Initial commit

---
+++ av2e3/src/ListaEncadeadaAntiga.java
@@ -0,0 +1,58 @@
+class No {
+    String dados;
+    No proximo;
+
+    public No(String dados){
+        this.dados = dados;
+        this.proximo = null;
+    }
+}
+
+public class ListaEncadeadaAntiga {
+    No cabeca;
+
+    public ListaEncadeadaAntiga(){
+        this.cabeca = null;
+    }
+
+    public void inserir(String dados){
+        No novoNo = new No(dados);
+
+        if (cabeca == null){
+            cabeca = novoNo;
+        }
+
+        else {
+            No atual = cabeca;
+            while (atual.proximo != null){
+                atual = atual.proximo;
+            }
+
+            atual.proximo = novoNo;
+        }
+    }
+
+    public void imprimir(){
+
+        No atual = cabeca;
+
+        while (atual != null){
+            System.out.print(atual.dados + " ");
+            atual = atual.proximo;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListaEncadeadaAntiga l1 = new ListaEncadeadaAntiga();
+
+        System.out.print("Lista encadeada: ");
+
+        l1.inserir("1");
+        l1.inserir("2");
+        l1.inserir("Oi");
+        l1.imprimir();
+
+    }
+}
---
+++ av2e3/src/Pilha.java
@@ -0,0 +1,86 @@
+import java.util.EmptyStackException;
+
+class NoPilha {
+    String dados;
+    NoPilha proximo;
+
+    public NoPilha(String dados) {
+        this.dados = dados;
+        this.proximo = null;
+    }
+}
+
+class Pilha {
+
+    NoPilha topo;
+
+    public Pilha() {
+        this.topo = null;
+    }
+
+    public void push(String dados){
+        NoPilha novoNo = new NoPilha(dados);
+
+        if (topo == null){
+            topo = novoNo;
+        } else {
+            novoNo.proximo = topo;
+            topo = novoNo;
+        }
+    }
+
+    public String pop(){
+        if (topo == null){
+            throw new EmptyStackException();
+        }
+
+        String dados = topo.dados;
+        topo = topo.proximo;
+        return dados;
+    }
+
+    public String peek(){
+        if (topo == null){
+            throw new EmptyStackException();
+        }
+
+        return topo.dados;
+    }
+
+    public void imprimirPilha() {
+        NoPilha atual = topo;
+
+        System.out.println("Elementos da pilha: \n");
+
+        while (atual != null) {
+            System.out.print(atual.dados + " ");
+            atual = atual.proximo;
+        }
+    }
+
+    public boolean estaVazia(){
+        return topo == null;
+    }
+
+    public static void main(String[] args) {
+        Pilha p1 = new Pilha();
+
+        p1.push("1");
+        p1.push("2");
+        p1.push("3");
+
+        System.out.println("Topo da pilha: " + p1.peek() + "\n");
+
+        System.out.println("Antes do pop: " + "\n");
+        p1.imprimirPilha();
+
+        System.out.println("\n");
+
+        while (!p1.estaVazia()){
+            System.out.println("Pop: " + p1.pop());
+        }
+
+        System.out.println("\nDepois do pop: \n");
+        p1.imprimirPilha();
+    }
+}
